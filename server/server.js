7567a80c416e70a070e582100ed627b166261553
Johng117 <83654331+Johng117@users.noreply.github.com>
2022-05-13 15:47:45 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -23,6 +23,6 @@ app.get("/district", (req, res) => {
   }
 });
 
-const listener = app.listen(process.env.PORT || 80, function () {
+const listener = app.listen(process.env.PORT || 3000, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
