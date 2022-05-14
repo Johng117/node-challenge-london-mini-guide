5f8116b6a171c07ff0fce9bc87025b38dbc899b4
Johng117 <83654331+Johng117@users.noreply.github.com>
2022-05-14 16:20:54 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const heathrowData = require("./client/data/Heathrow.json");
 const stratfordData = require("./client/data/Stratford.json");
 app.use(cors());
 app.use(express.json());
+app.use(express.static("./client/public"));
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello John!" });
@@ -24,8 +25,6 @@ app.get("/district", (req, res) => {
     res.json(allDistricts[district]);
   }
 });
-
-app.use(express.static(path.join(__dirname, "../client/build")));
 
 const listener = app.listen(process.env.PORT || 3001, function () {
   console.log("Your app is listening on port " + listener.address().port);
