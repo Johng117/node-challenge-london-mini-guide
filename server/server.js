bb081ccb63ad4401c411b2a70af599148e53fbb2
Johng117 <83654331+Johng117@users.noreply.github.com>
2022-05-14 13:02:22 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.get("/district", (req, res) => {
   }
 });
 
-app.use(express.static(path.join(__dirname, "../client/public")));
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 const listener = app.listen(process.env.PORT || 3001, function () {
   console.log("Your app is listening on port " + listener.address().port);
